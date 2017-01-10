ddeb6134fa0057fb30d08cc789f19ef4eaab2158
darkf <lw9k123@gmail.com>
2017-01-10 00:04:42 -0800

    add docstring to _

--- darkf.py
+++ darkf.py
@@ -4,6 +4,16 @@
 # NOTE: _ will have to be imported explicitly, if desired.
 
 class UnderscoreProxy:
+    """A proxy object that returns accessors for attributes and
+       items looked up on it.
+
+       Example: _.foo is the same as lambda x: x.foo
+                _["foo"] is the same as lambda x: x["foo"]
+
+       It is useful for producing higher-order functions with nicer
+       syntax than operator.itemgetter.
+    """
+
     def __getattr__(self, attr):
         return lambda x: getattr(x, attr)
     
--- test_darkf.py
+++ test_darkf.py
@@ -9,11 +9,11 @@ from darkf import _
 from collections import namedtuple
 
 def test_underscore():
-	nt = namedtuple('nt', 'x y foo')
+    nt = namedtuple('nt', 'x y foo')
 
-	# attribute accessors
-	assert _.foo(nt('derp', 1, 42)) == 42
+    # attribute accessors
+    assert _.foo(nt('derp', 1, 42)) == 42
 
-	# item accessors
-	assert _[0]([42, 1, 2, 3]) == 42
-	assert _['foo']({"foo": 42}) == 42
+    # item accessors
+    assert _[0]([42, 1, 2, 3]) == 42
+    assert _['foo']({"foo": 42}) == 42

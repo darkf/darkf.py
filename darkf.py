119365577cf5265de8b6b46366f6b23b6c773ba2
darkf <lw9k123@gmail.com>
2017-01-10 00:02:37 -0800

    initial commit

---
+++ darkf.py
@@ -0,0 +1,13 @@
+# darkf.py -- a supplemental standard library by darkf
+# Copyright (c) 2017 darkf -- licensed under the terms of the MIT license
+#
+# NOTE: _ will have to be imported explicitly, if desired.
+
+class UnderscoreProxy:
+    def __getattr__(self, attr):
+        return lambda x: getattr(x, attr)
+    
+    def __getitem__(self, item):
+        return lambda x: x[item]
+
+_ = UnderscoreProxy()
---
+++ test_darkf.py
@@ -0,0 +1,19 @@
+# Test suite for darkf.py
+# Copyright (c) 2017 darkf -- licensed under the terms of the MIT license
+#
+# Uses py.test -- pip install pytest
+# then $ py.test
+
+from darkf import *
+from darkf import _
+from collections import namedtuple
+
+def test_underscore():
+	nt = namedtuple('nt', 'x y foo')
+
+	# attribute accessors
+	assert _.foo(nt('derp', 1, 42)) == 42
+
+	# item accessors
+	assert _[0]([42, 1, 2, 3]) == 42
+	assert _['foo']({"foo": 42}) == 42
